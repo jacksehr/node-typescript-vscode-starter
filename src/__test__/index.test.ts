3deb946e54ef9c61a83574f826ce4c8d8e349dc9
Jack She <jshe@atlassian.com>
2021-07-20 19:21:35 +1000

    regression logic doable

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,7 +1,23 @@
 import { main } from '../index';
 
+const x = [0.5, 1, 1.5, 2, 2.5];
+const y = [0, 1, 2, 3, 4];
+
+const mockData = x.map((xValue, index) => ({
+  x: xValue,
+  y: y[index]
+}));
+
+jest.mock('node-fetch', () => ({
+  __esModule: true,
+  default: async () => ({ json: async () => mockData })
+}))
+
 describe('index.ts', () => {
-  it('should work correctly', () => {
-    expect(main()).toEqual('Hello world!');
+  it('should correctly run regression', async () => {
+    const regressionResult = await main();
+
+    expect(regressionResult.intercept).toEqual(-1);
+    expect(regressionResult.slope).toEqual(2);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-const main = () => {
-  return `Hello world!`;
-}
+import fetch from "node-fetch";
+import SimpleLinearRegression from "ml-regression-simple-linear";
 
-main();
+const DATA_ENDPOINT = "https://some-url";
+
+const transformApiResponse = (
+  data: { x: number; y: number }[]
+): [number[], number[]] => {
+  return [data.map(({ x }) => x), data.map(({ y }) => y)];
+};
+
+const main = async () => {
+  const response = await fetch(DATA_ENDPOINT);
+  const data = await response.json();
+
+  const [xValues, yValues] = transformApiResponse(data);
+
+  return new SimpleLinearRegression(xValues, yValues);
+};
 
 export { main };
